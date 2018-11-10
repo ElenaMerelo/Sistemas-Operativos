fb6b6f6bcc837b4d156f499f6724944ff1537eb3
elenamerelo <nenamer@gmail.com>
2018-11-10 16:50:15 +0100

    Pequeña mejora, sigue dando violación de segmento pero al rato

--- prácticas_módulo2/Sesión2/ejer3.c
+++ prácticas_módulo2/Sesión2/ejer3.c
@@ -33,10 +33,11 @@ un directorio dentro hace lo mismo, y así sucesivamente hasta que no haya más 
 ni archivos. Hacemos los punteros constantes para que no se pueda modificar el lugar al
 que apuntan pero sí a lo que apuntan.
 */
-void info_dir(const char *pathname, int * const files, long long int * const total_size){
+void info_dir(const char *pathname, int * const reg_files, long long int * const total_size, long long int *const reg_size){
   DIR *dir= NULL;
   struct dirent *dp= NULL;
   struct stat atributes;
+  char name[500];
 
   dir= opendir(pathname);
   //Abrimos el flujo del directorio pasado como argumento
@@ -46,56 +47,67 @@ void info_dir(const char *pathname, int * const files, long long int * const tot
     exit(-1);
   }
 
+
+
   /*Recorremos ahora lo que hay en el directorio
   Con S_IXGRP  comprobamos si el grupo tiene permisos de ejecución
   Con  S_IXOTH comprobamos si otros tienen permiso de ejecución
   Con S_ISREG(mode) vemos si el archivo es regular, si lo es incrementamos el contado de archivos regulares
   */
   while((dp= readdir(dir)) != NULL){
-    if(stat(dp->d_name, &atributes) < 0){
-      printf("\nError al intentar acceder a los atributos de %s\n", dp->d_name);
-      perror("\nError en stat\n");
-      exit(-1);
-    }
-    if(dp->d_name[0] != '.' && dp->d_name[strlen(dp->d_name)-1] != '~'){
+    //Si no es un archivo oculto
+    if(dp->d_name[0] != '.' && strcmp(dp->d_name, "..") != 0 && dp->d_name[strlen(dp->d_name)-1] != '~'){
+      strcpy(name, pathname);
+      strcat(name, "/");
+      strcat(name,dp->d_name);
+
+      if(stat(name, &atributes) < 0){
+        printf("\nError al intentar acceder a los atributos de %s\n", dp->d_name);
+        perror("\nError en stat\n");
+        // exit(-1);
+      }
+
+      (*total_size) += atributes.st_size;
+
       if(S_ISREG(atributes.st_mode)){ //Si el archivo es regular
-        //printf("\n%s es un archivo regular", dp->d_name);
+        printf("\n%s es un archivo regular", dp->d_name);
+
         if(atributes.st_mode & (S_IXGRP | S_IXOTH)){  //Si group y others tienen permiso de ejecución
           printf("\n%s es un archivo regular y tiene permisos de ejecución para others y group", dp->d_name);
-          (*files)++;
-          (*total_size) += atributes.st_size;
+          (*reg_files)++;
+          (*reg_size) += atributes.st_size;
           printf("\n Nombre: %s, i-nodo: %d", dp->d_name, dp->d_ino);
-          printf("\nNúmero de archivos regulares: %d, total de bytes: %lld", &files, &total_size);
         }
-        //else
-          //printf("\n%s no tiene permisos de ejecución para others y group", dp->d_name);
+        else
+          printf("\n%s no tiene permisos de ejecución para others y group", dp->d_name);
       }
+
       //Si es directorio y no es un archivo oculto
       else if(S_ISDIR(atributes.st_mode)){ //Si es un directorio
         printf("\n%s es un directorio", dp->d_name);
-        info_dir(dp->d_name, &files, &total_size);  //Nos metemos dentro de él, y realizamos lo mismo que en el directorio padre
+        info_dir(dp->d_name, &reg_files, &total_size, &reg_size);  //Nos metemos dentro de él, y realizamos lo mismo que en el directorio padre
       }
-      //else
-        //printf("\n%s no es un archivo regular ni un directorio", dp->d_name);
-
     }
+    else
+      printf("\n%s no es un archivo regular ni un directorio", dp->d_name);
   }
   closedir(dir);
 }
 
 int main(int argc, char *argv[]){
-  int files= 0;
-  long long int total_size= 0;
+  int reg_files= 0;
+  long long int total_size= 0, reg_size= 0;
+
   if(argc > 2){
-    printf("\nModo de ejecución: ./ejer3 ./directorio\n");
+    printf("\nModo de ejecución: ./ejer3 (./directorio)\n");
     exit(-1);
   }
 
   if(argc == 2)
-    info_dir(argv[1], &files, &total_size);
+    info_dir(argv[1], &reg_files, &total_size, &reg_size);
   else
-    info_dir(get_current_dir_name(), &files, &total_size);
+    info_dir(".", &reg_files, &total_size, &reg_size);
 
-  printf("\nExisten %d archivos regulares con permisos de ejecución para group y others\n", files);
-  printf("\nEl tamaño total ocupado por dichos archivos es %d bytes\n", total_size);
+  printf("\nExisten %d archivos regulares con permisos de ejecución para group y others\n", reg_files);
+  printf("\nEl tamaño total ocupado por archivos regulares es %lld bytes y por el resto de archivos %lld\n", reg_size, total_size);
 }
