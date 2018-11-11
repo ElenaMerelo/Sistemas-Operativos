877ba5ea144f4acae5db5e8df822c390bad9af84
elenamerelo <nenamer@gmail.com>
2018-11-11 16:33:37 +0100

    Minoriiisimos changes

--- prácticas_módulo2/Sesión4/tarea6.c
+++ prácticas_módulo2/Sesión4/tarea6.c
@@ -9,6 +9,8 @@ POSIX 2.10
 #include<stdio.h>
 #include<stdlib.h>
 #include<errno.h>
+#include <string.h>
+
 int main(int argc, char *argv[]){
   int fd[2], numBytes;
   pid_t PID;
@@ -21,16 +23,17 @@ int main(int argc, char *argv[]){
     exit(-1);
   }
 
-  if (PID == 0) { //Cierre del descriptor de lectura en el proceso hijo
+  if (PID == 0) {
+    //Cierre del descriptor de lectura en el proceso hijo, porque vamos a escribir
     close(fd[0]);
     // Enviar el mensaje a través del cauce usando el descriptor de escritura
     write(fd[1],mensaje,strlen(mensaje)+1);
     exit(0);
   }
   else { // Estoy en el proceso padre porque PID != 0
-    //Cerrar el descriptor de escritura en el proceso padre
+    //Cerrar el descriptor de escritura en el proceso padre, porque vamos a leer
     close(fd[1]);
-    //Leer datos desde el cauce
+    //Leer los datos que ha mandado el hijo desde el cauce
     numBytes= read(fd[0],buffer,sizeof(buffer));
     printf("\nEl número de bytes recibidos es: %d",numBytes);
     printf("\nLa cadena enviada a través del cauce es: %s", buffer);
