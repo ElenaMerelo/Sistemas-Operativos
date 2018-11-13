9650a4df45af0e0cbc0069468d1daefc96623c74
elenamerelo <nenamer@gmail.com>
2018-11-13 09:46:40 +0100

    Añadidos ejemplos de la sesión 6

---
+++ prácticas_módulo2/Sesión6/tarea17.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <stdlib.h>
+
+
+int main (int argc, char *argv[]) {
+  char *p;
+
+  p = (char *) mmap (0, sizeof(char), PROT_READ, MAP_SHARED | MAP_ANON, -1, 0);
+  if (p == MAP_FAILED) {
+       printf("Falló el mapeo\n");
+       return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
